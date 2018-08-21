35e5726aa92e24779a22a54ef378e75ca0d24410
yangjing <yangjingpuyu@aliyun.com>
2018-08-21 10:49:17 +0800

    init

---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import HelloWorld from '../components/HelloWorld'
+export const otherRoutes = {
+  path: '/',
+  name: 'HelloWorld',
+  component: HelloWorld
+}
+
+export const routes = [
+  otherRoutes
+]
